caa9705e1a40e057b9ab1d5a02f32d901930879b
John Dillinger <higz-06@hotmail.com>
2018-10-20 10:50:07 +0100

    /timetable fixes

--- router/routes.js
+++ router/routes.js
@@ -5,7 +5,7 @@ const roomChecker = require('../utils/roomChecker');
 const router = new KoaRouter();
 
 router.get('/timetable', async (ctx) => {
-  ctx.body = 'example /api/SG_KSDEV_B07-F-Y2-1-(A)';
+  ctx.body = 'example /timetable/SG_KSDEV_B07-F-Y2-1-(A)';
 });
 
 router.get('/timetable/:code', async (ctx) => {
