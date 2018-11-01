1f89e4cd568be5100e941f64ba42664eb861c99e
John Dillinger <higz-06@hotmail.com>
2018-11-01 17:46:58 +0000

    refactoring

--- src/utils/intToDay.js
+++ src/utils/intToDay.js
@@ -1,26 +1,4 @@
 module.exports = (i) => {
-  let day = '';
-
-  switch (i) {
-    case 1:
-      day = 'monday';
-      break;
-    case 2:
-      day = 'tuesday';
-      break;
-    case 3:
-      day = 'wednesday';
-      break;
-    case 4:
-      day = 'thursday';
-      break;
-    case 5:
-      day = 'friday';
-      break;
-    default:
-      day = 'invalid';
-      break;
-  }
-
-  return day;
+  const arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  return arr[i];
 };
--- src/utils/timeTableChecker.js
+++ src/utils/timeTableChecker.js
@@ -1,24 +1,30 @@
 const urlFactory = require('./urlFactory');
 const tabletojson = require('tabletojson');
+const getDay = require('./intToDay');
 
 module.exports = async (courseCode) => {
-  const json = await tabletojson.convertUrl(urlFactory(courseCode));
+  let json = await tabletojson.convertUrl(urlFactory(courseCode));
 
   const jsonObj = {};
   jsonObj.data = [];
 
-  for (let d = 8; d < 13; d += 1) {
+  json = json.splice(8, 5);
+
+  for (let d = 0; d < json.length; d += 1) {
     jsonObj.data.push([]);
 
     for (let entry = 1; entry < json[d].length; entry += 1) {
-      if (json[d][entry][2] === '' || json[d][entry][5] === '' || json[d][entry][0] === '') { continue; }
-      jsonObj.data[d - 8].push({
-        name: json[d][entry][0],
-        type: json[d][entry][2],
+      if (json[d][entry][2] === '' || json[d][entry][5] === '' || json[d][entry][0] === '') continue;
+
+      jsonObj.data[d].push({
+        day: getDay(d),
         startTime: json[d][entry][3],
-        endTime: json[d][entry][4],
-        length: json[d][entry][5],
+        name: json[d][entry][0],
         room: json[d][entry][7],
+        type: json[d][entry][2],
+        teacher: json[d][entry][8],
+        length: json[d][entry][5],
+        endTime: json[d][entry][4],
       });
     }
   }
