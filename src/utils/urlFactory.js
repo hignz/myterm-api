fdc4f2e7294a59c7ee20a75a73cc2a613739cb3d
hignz <higz-06@hotmail.com>
2018-11-13 21:15:51 +0000

    cleanup

--- src/utils/urlFactory.js
+++ src/utils/urlFactory.js
@@ -1,1 +1,2 @@
-module.exports = urlPart => `http://timetables.itsligo.ie:81/reporting/textspreadsheet;student+set;id;${urlPart}?t=student+set+textspreadsheet&days=1-16&weeks=&periods=3-20&template=student+set+textspreadsheet`;
+module.exports = urlPart => 'http://timetables.itsligo.ie:81/reporting/textspreadsheet;student+set;id;'
+  + `${urlPart}?t=student+set+textspreadsheet&days=1-16&weeks=&periods=3-20&template=student+set+textspreadsheet`;
