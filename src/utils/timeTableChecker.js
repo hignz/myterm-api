6d8e9d9bd0a1a2d45f712e62c4bf39fa2ffdb017
hignz <higz-06@hotmail.com>
2018-11-25 22:36:14 +0000

    added url and course code to json, refactoring

--- src/utils/timeTableChecker.js
+++ src/utils/timeTableChecker.js
@@ -3,41 +3,41 @@ const cheerio = require('cheerio');
 const got = require('got');
 
 module.exports = async (urlPart) => {
-  const { body } = await got(urlFactory(urlPart));
+  const url = urlFactory(urlPart);
+  const { body } = await got(url);
   const $ = cheerio.load(body);
-  const tables = $('table').filter(function () { return $(this).parents('table').length === 0; }).slice(1, -1); // Only top level tables
+
+  const tables = $('table') // Only top level tables
+    .filter((i, elm) => (!$(elm).parents('table').length))
+    .slice(1, -1);
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
   const jsonObj = {};
   jsonObj.data = [];
 
-  $(tables).each((h, e) => { // Day
+  $(tables).each((i, table) => { // Day
     jsonObj.data.push([]);
-    $(e).children('tbody:not(:nth-child(1))').each((i, elm) => { // Only days with class
-      $(elm).find('tr').each((j, elmem) => { // Class
-        if (j === 0) return; // Skip row with useless data
-        const module = $(elmem);
-        const details = [];
-        $(module).find('td').each((k, elmm) => {
-          details.push($(elmm).text());
-        });
-        jsonObj.data[h].push({
-          day: days[h],
-          startTime: details[3],
-          name: details[0],
-          room: details[7].trim() || 'N/A',
-          type: details[2],
-          teacher: details[8],
-          length: details[5],
-          endTime: details[4],
-        });
+    $(table).children('tbody').find('tr').each((j, row) => { // Only days with class
+      if (j === 0) return; // Skip row with useless data
+      const details = [];
+      $(row).find('td').each((k, cell) => {
+        details.push($(cell).text());
+      });
+      jsonObj.data[i].push({
+        day: days[i],
+        startTime: details[3],
+        name: details[1].split('- ')[1],
+        room: details[7].trim() || 'N/A',
+        type: details[2],
+        teacher: details[8],
+        length: details[5],
+        endTime: details[4],
       });
     });
   });
 
-  const isEmpty = a => Array.isArray(a) && a.every(isEmpty);
-
-  jsonObj.empty = isEmpty(jsonObj.data);
+  jsonObj.courseCode = decodeURIComponent(urlPart);
+  jsonObj.url = url;
 
   console.log(urlFactory(urlPart));
 
