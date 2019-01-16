bf2a740c451ed564cf203df1a75478e9d0e93bdb
hignz <higz-06@hotmail.com>
2019-01-16 01:02:53 +0000

    fixes (potentially broke everything)

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   URL_START: 'http://timetables.itsligo.ie:81/reporting/textspreadsheet;student+set;id;',
   SEM1_URL_PART: '?t=student+set+textspreadsheet&days=1-7&weeks=3-9;11-16&periods=1-28&template=student+set+textspreadsheet',
-  SEM2_URL_PART: '?t=student+set+textspreadsheet&days=1-7&weeks=22-33;36&periods=1-28&template=student+set+textspreadsheet',
+  SEM2_URL_PART: '?t=student+set+textspreadsheet&days=1-7&weeks=22&periods=1-28&template=student+set+textspreadsheet',
 };
