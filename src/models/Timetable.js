3f914b42780dc5d1760170e3f7254b97ffe8f87f
hignz <higz-06@hotmail.com>
2019-06-29 02:23:42 +0100

    improved Timetable model

--- src/models/Timetable.js
+++ src/models/Timetable.js
@@ -1,12 +1,37 @@
 const mongoose = require('mongoose');
 
+const Day = new mongoose.Schema({
+  day: String,
+  startTime: String,
+  name: String,
+  room: String,
+  type: String,
+  teacher: String,
+  length: String,
+  endTime: String,
+}, { _id: false });
+
 const TimetableSchema = new mongoose.Schema({
   course: {
+    type: String,
+  },
+  data: [[Day]],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  empty: {
+    type: Boolean,
+  },
+  courseCode: {
     type: String,
   },
   url: {
     type: String,
   },
-}, { collection: 'data' });
+  semester: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model('Timetable', TimetableSchema);
