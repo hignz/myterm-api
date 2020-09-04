d1d15ac3df2426cba6e2c847d6f49bcf130fd455
hignz <higz-14@live.com>
2020-09-05 00:31:03 +0100

    validate sem ads number

--- src/validations/timetable.validation.js
+++ src/validations/timetable.validation.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const getTimetable = Joi.object({
-  code: Joi.string().max(50).required(),
+  code: Joi.string().max(75).required(),
   college: Joi.number().min(0).max(2).default(0).required(),
-  sem: Joi.string(),
+  sem: Joi.number().min(0).max(1),
 });
 
 module.exports = {
